ed415f91fadeb613c64cc4e9919979d8a2ead5e5
lmcarroll <liamcarrollapplicant@gmail.com>
2021-09-26 17:04:31 -0400

    Initial Commit

---
+++ src/main/java/ex39/App.java
@@ -0,0 +1,32 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Liam Carroll
+ */
+
+package ex39;
+
+import java.util.Arrays;
+
+public class App {
+
+    static void printAlphabetical(Records[] arr) {
+
+        System.out.printf("%-20s| %-17s| %-10s\n", "Name", "Position", "Separation Date");
+        System.out.println("--------------------|------------------|----------------");
+        for (Records records : arr) {
+            records.printRecords();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Records johnson = new Records("John", "Johnson", "Manager", "2016-12-31");
+        Records xiong = new Records("Tou", "Xiong", "Software Engineer", "2016-10-05");
+        Records michaelson = new Records("Michaela", "Michaelson", "District Manager", "2015-12-19");
+        Records jacobson = new Records("Jake", "Jacobson", "Programmer", "");
+        Records jackson = new Records("Jacquelyn", "Jackson", "DBA", "");
+        Records webber = new Records("Sally", "Webber", "Web Developer", "2015-12-18");
+        Records[] arr = {jackson, jacobson, johnson, michaelson, webber, xiong};
+        printAlphabetical(arr);
+    }
+}
---
+++ src/main/java/ex39/Records.java
@@ -0,0 +1,40 @@
+package ex39;
+
+public class Records {
+
+    String firstName;
+    String lastName;
+    String position;
+    String separationDate;
+
+    public Records(String firstName, String lastName, String position, String separationDate) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.position = position;
+        this.separationDate = separationDate;
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getSeparationDate() {
+        return separationDate;
+    }
+
+    public void printRecords () {
+
+        System.out.printf("%-9s %-10s| %-17s| %-10s\n", this.getFirstName(), this.getLastName(), this.getPosition(), this.getSeparationDate());
+    }
+
+}
